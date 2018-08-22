d30cb6a5ebf03882b2b8eac1268385372db64cd7
Andó Botár <andobotar@yahoo.com>
2018-08-22 13:52:38 +0200

    remove redundant websocket import, code and packages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
-// import SockJsClient from 'react-stomp';
 import AjaxDemo from "./components/AjaxDemo";
 
 class App extends Component {
-
     render() {
         return (
             <div className="App">
-
                 <AjaxDemo />
-
-                {/*<SockJsClient
-                    url='http://localhost:8080/handler' topics={['/topic/kutza']}
-                    onMessage={(msg) => { console.log(msg); }}
-                    // ref={ (client) => { this.clientRef = client }}
-                />*/}
-
             </div>
         );
     }
